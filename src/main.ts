db4575417cb8981d092a60de7fefb5ff09f3b34f
B00ttl3 <mumair3825@gmail.com>
2025-03-23 03:03:55 +0500

    added new paths

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes, withHashLocation } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  // Define your routes here if needed
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withHashLocation()) // Enables Hash-based Routing
+  ]
+}).catch(err => console.error(err));
